f5e4f9fc91d83b6e132f6c0801d9343c5cbf7de3
Cristiana Raducanu <cristinicardcn@gmail.com>
2025-01-17 19:23:22 +0200

    Added problem solution, target market, primary functions and some other changes and styling changes

--- EcoCorp/src/app/pages/landing-page/landing-target-market/landing-target-market.component.ts
+++ EcoCorp/src/app/pages/landing-page/landing-target-market/landing-target-market.component.ts
@@ -8,19 +8,29 @@ import { Component } from '@angular/core';
 export class LandingTargetMarketComponent {
   targets = [
     {
-      title: "Corporations",
-      description: "Private corporations with disused equipment",
-      image: "./assets/target-market/Corporations.svg"
+      title: "Manageri și Executivi",
+      description: "Profesioniști ocupați, responsabili de coordonarea echipelor, planificarea și gestionarea sarcinilor.",
+      image: "./assets/target-market/management.png"
     },
     {
-      title: "Government",
-      description: "Confiscated items by the government",
-      image: "./assets/target-market/Government.svg"
+      title: "Studenți",
+      description: "Tineri organizați sau în căutarea unor metode mai eficiente de gestionare a timpului pentru studiu.",
+      image: "./assets/target-market/students.png"
     },
     {
-      title: "Individuals",
-      description: "Individuals who want to sell old belonging",
-      image: "./assets/target-market/Individuals.svg"
+      title: "Creatori de Conținut",
+      description: "Persoane creative care lucrează la proiecte multimedia cu un flux de lucru dinamic.",
+      image: "./assets/target-market/influencer.png"
+    },
+    {
+      title: "Utilizatori Casnici",
+      description: "Persoane care își simplifică viața zilnică prin organizarea activităților și utilizarea dispozitivelor inteligente.",
+      image: "./assets/target-market/location.png"
+    },
+    {
+      title: "Corporate",
+      description: "Organizații care optimizează procesele interne și gestionează eficient resursele.",
+      image: "./assets/target-market/corporation.png"
     },
   ]
 }
